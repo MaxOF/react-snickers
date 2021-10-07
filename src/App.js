4695e45a8e53b6d4dd56919206e9bdc7fc4437a6
Max <egorovteam2019@yandex.ru>
2021-10-07 21:58:23 +0300

    mockApi

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
 
-const arr = [
-  { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: '/img/snickers/1.jpg' },
-  { title: 'Мужские Кроссовки Nike Air Max 270', price: 12999, imageUrl: '/img/snickers/2.jpg' },
-  { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 8499, imageUrl: '/img/snickers/3.jpg' },
-  { title: 'Кроссовки Puma X Aka Boku Future Rider', price: 8999, imageUrl: '/img/snickers/4.jpg' },
-];
 
 function App() {
+  const [items, setItems] = React.useState([]);
   const [cartOpened, setCartOpened] = React.useState(false);
+
+  React.useEffect(() => {
+    fetch('https://615f36b4f7254d0017068056.mockapi.io/items').then(res => {
+      return res.json();
+    }).then(json => {
+      setItems(json);
+    })
+  }, []);
 
   return (
     <div className="wrapper clear">
@@ -25,8 +28,8 @@ function App() {
             <input placeholder="Поиск ..." />
           </div>
         </div>
-        <div className="d-flex">
-          {arr.map((obj) => (
+        <div className="d-flex flex-wrap">
+          {items.map((obj) => (
             <Card
               title={obj.title}
               price={obj.price}
